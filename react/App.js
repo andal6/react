f27985e2ec85682c10551a72372ac393e2339b50
andal6 <andalthirumalai5@gmail.com>
2025-07-15 00:11:22 +0530

    episode1

---
+++ react/App.js
@@ -0,0 +1,22 @@
+const parent=React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child"}, [
+    React.createElement("h1",{},"Im an h1 tag"),
+    React.createElement("h2",{},"Im an h2 tag")
+    ]),
+    React.createElement("div",{id:"child2"},[
+    React.createElement("h1",{},"Im an h1 tag"),
+    React.createElement("h2",{},"Im an h2 tag")
+    ]),
+]);
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+console.log(parent)
+
+// const heading=React.createElement("h1",{id:"heading"},"Hello world from react");
+//  const root=ReactDOM.createRoot(document.getElementById("root"));
+//  root.render(heading);
+
+//  console.log(heading);
+
